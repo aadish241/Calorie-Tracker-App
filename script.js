a21aaa1d94437048049e197640b641caaeca370b
Aadish Jain <aadish241@gmail.com>
2025-07-07 18:34:12 +0530

    third commit

--- script.js
+++ script.js
@@ -2,17 +2,28 @@ function calcalcfunc(){
     document.getElementById("calorie-calc-page").style.display = "block";
     document.getElementById("calorie-count-page").style.display = "none";
     document.getElementById("meal-plan-page").style.display = "none";
+    document.getElementById("calcalcbtn").style.boxShadow= "1px 1px 8px";
+    document.getElementById("calcountbtn").style.boxShadow= "0px 0px 0px";
+    document.getElementById("mealplanbtn").style.boxShadow= "0px 0px 0px";
+
 
 }
 function calcountfunc(){
     document.getElementById("calorie-calc-page").style.display = "none";
     document.getElementById("calorie-count-page").style.display = "block";
     document.getElementById("meal-plan-page").style.display = "none";
+    document.getElementById("calcalcbtn").style.boxShadow= "0px 0px 0px";
+    document.getElementById("calcountbtn").style.boxShadow= "1px 1px 8px";
+    document.getElementById("mealplanbtn").style.boxShadow= "0px 0px 0px";
 }
 function mealplanfunc(){
     document.getElementById("calorie-calc-page").style.display = "none";
     document.getElementById("calorie-count-page").style.display = "none";
     document.getElementById("meal-plan-page").style.display = "block";
+    document.getElementById("calcalcbtn").style.boxShadow= "0px 0px 0px";
+    document.getElementById("calcountbtn").style.boxShadow= "0px 0px 0px";
+    document.getElementById("mealplanbtn").style.boxShadow= "1px 1px 8px";
+    
 }
 let weight;
 let height;
@@ -42,4 +53,24 @@ fbmr=Number(fbmr);
 document.getElementById("calresult").textContent= `Your maintenance calories are: ${fbmr*1.375} cal`;
 document.getElementById("weightlosscal").textContent= ` ${fbmr*1.375 - 500} cal`;
 document.getElementById("weightgaincal").textContent= ` ${fbmr*1.375 + 300} cal`;
+}
+
+let count = 0;
+document.getElementById("addcalbtn").onclick= function(){
+let amount= document.getElementById("cal+").value;
+amount=Number(amount);
+count+=amount;
+document.getElementById("actual-count").textContent= count;
+}
+
+document.getElementById("subcalbtn").onclick= function(){
+let amount= document.getElementById("cal-").value;
+amount=Number(amount);
+count-=amount;
+document.getElementById("actual-count").textContent= count;
+}
+
+document.getElementById("resetbtn").onclick= function(){
+    count=0;
+    document.getElementById("actual-count").textContent= count;
 }
